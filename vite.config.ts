c080e37cbaf239304d488187a3dd8e1e86f164e6
kimistores <dev@kimistores.com>
2025-01-03 04:57:49 +0700

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,16 @@
-import { defineConfig, Plugin } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import checker from 'vite-plugin-checker';
+import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
-import { extname, relative, resolve } from 'path';
-import { fileURLToPath } from 'node:url';
-import { glob } from 'glob';
-import preserveDirectives from 'rollup-preserve-directives';
+import type { Plugin } from 'vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react-swc';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import preserveDirectives from 'rollup-preserve-directives';
 //----------------------------------------------------------------------
 
 const PORT = 8080;
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      formats: ['es'],
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      },
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        glob
-          .sync('lib/**/*.{ts,tsx}', {
-            ignore: ['lib/**/*.d.ts'],
-          })
-          .map((file: any) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative('lib', file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-    },
-  },
   plugins: [
     react(),
     libInjectCss(),
